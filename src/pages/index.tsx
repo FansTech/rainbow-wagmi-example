60bf421718e08c83df9c0febe95e07f91c3692d0
fleksin <hezhoufan@gmail.com>
2024-12-18 11:39:03 +0800

    add sth else

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,8 @@ import Link from 'next/link'
 
 export default function IndexPage() {
     return (<>
-        <Link href='./rainbowkit'>rainbow</Link>
+        <Link href='./rainbowkit'>rainbow ↗</Link>
         <br/>
-        <Link href='./wagmi'>wagmi</Link>
+        <Link href='./wagmi'>wagmi ↗</Link>
     </>)
 }
